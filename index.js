b492a70f9950d26412af84dec16e46a1ccd0bb8b
DianaAraujoG <dianac251@gmail.com>
2021-10-05 11:20:45 -0500

    callback igualado a algo para ver si retorna

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const route = process.argv[2];//__dirname; // la ruta absoluta de donde estoy
 
 let arrayFiles = [];
 
-files.searchFiles(route, (err,  content)=>{
+const prueba = files.searchFiles(route, (err,  content)=>{
   if (err){ console.log(err) }
   arrayFiles.push(content);
   console.log(arrayFiles);
